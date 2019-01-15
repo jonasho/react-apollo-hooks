053c317f1f65f7340e88635996c396f4efbd57d3
Jonas Holtorf <jonas.holtorf@gmail.com>
2019-01-15 17:31:52 +0100

    feat: temporary add compiled version to git for easy install

---
+++ lib/utils.d.ts
@@ -0,0 +1,7 @@
+import { ExecutionResult } from 'graphql/execution/execute';
+export declare type FetchResult<TData, C = Record<string, any>, E = Record<string, any>> = ExecutionResult<TData> & {
+    extensions?: E;
+    context?: C;
+};
+export declare type Omit<T, K extends keyof T> = Pick<T, Exclude<keyof T, K>>;
+export declare function objToKey<T extends Record<string, any>>(obj: T): T | string;
